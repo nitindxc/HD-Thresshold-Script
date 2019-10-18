8778da8cfbe74f6902e79e401d8fc8b2e17de079
Chudasama <nchudasama2@dxc.com>
2019-10-18 15:03:18 +0100

    Initial PS script to obtain PC HD Threshold info

---
+++ HDThresholdScript.ps1
@@ -0,0 +1,10 @@
+﻿$comp = Read-Host 'Please Enter Server Name'
+Write-Host 'Testing Server Connection' -ForegroundColor Green
+if (test-connection $comp -ErrorAction SilentlyContinue){
+    Write-Host 'Collecting Drive Data' -ForegroundColor Green
+    Get-WmiObject -Class win32_logicaldisk -ComputerName $comp -Filter "DriveType='3'" | select DeviceID, @{n='Freespace (GB)'; e={"{0:n1}" -f ($_.freespace / 1GB)}}, @{n='Size (GB)'; e={"{0:n1}" -f ($_.size / 1GB)}}, @{n='Threshold (%)'; e={"{0:n1}" -f ($_.freespace / $_.size)}} | Format-Table -AutoSize
+}# End If
+Else {
+    Write-Host "Network Connetivity to the server $comp failed" -ForegroundColor red
+}# End Else
+$end = Read-Host 'Press any key to quite'
